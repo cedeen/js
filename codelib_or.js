9742651485d21c6de8e52df42eefe92ff22cd781
cedeen <idberselancar@gmail.com>
2016-10-04 15:09:56 +0700

    Update codelib_or.js

--- codelib_or.js
+++ codelib_or.js
@@ -1,8 +1,7 @@
-// Widget Techno Science 300x250
 var ins = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle"'+
      'style="display:inline-block;width:300px;height:250px"'+
-     'data-ad-client="ca-pub-1603158290565684"'+
-     'data-ad-slot="7345890659"></ins>'+
+     'data-ad-client="ca-pub-1822730405949039"'+
+     'data-ad-slot="3652928506"></ins>'+
 	'<script>'+
 	'(adsbygoogle = window.adsbygoogle || []).push({});'+
 	'<\/script>';
