85653af8001273a5a10e8415c8a54e196b7de7e5
cedeen <idberselancar@gmail.com>
2016-10-04 16:01:53 +0700

    Update codelib_or.js

--- codelib_or.js
+++ codelib_or.js
@@ -1,38 +1,39 @@
-//Right Top Sidebar 300x250 TechnoBub
-var ins = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle"' +
-    'style="display:inline-block;width:300px;height:250px"' +
-    'data-ad-client="ca-pub-1822730405949039"' +
-    'data-ad-slot="3652928506"></ins>' +
-    '<script>' +
-    '(adsbygoogle = window.adsbygoogle || []).push({});' +
-    '<\/script>';
-$(document).ready(function() {
-    $.getJSON("https://ip.nf/me.json", function(e) {
-	var n = e.ip.city;
-	e.ip.country, e.ip.country_code;
-	console.log(calcTime('Markas', '-7'));
-    })
-});
-function njedul(e) {
-    var n = e.match(/google/gi);
-    return n
-}
-
-function sapaDeke(kot,sapa){
-	if ((kot == 'Mountain View') || (sapa!=null)) {
-		$('.sapadeke').remove();
-		$('.topsidebar').html(ins);
-	  }else{
-		$('.sapadeke').html(ins);
-	  }
-}
-function calcTime(city, offset) {
-     d = new Date();
-     utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-     nd = new Date(utc + (3600000*offset));
-     jam = nd.getHours();
-     if (jam>19 && jam<6) {
-      sapaDeke(n, njedul(e.ip.asn))
-  }
-     return jam+"=> The local " + city + " is " + nd.toLocaleString();
-}
+var ins = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle"'+
+     'style="display:inline-block;width:300px;height:250px"'+
+     'data-ad-client="ca-pub-1822730405949039"'+
+     'data-ad-slot="3652928506"></ins>'+
+	'<script>'+
+	'(adsbygoogle = window.adsbygoogle || []).push({});'+
+	'<\/script>';
+	$(document).ready(function() {
+	    $.getJSON('//ip-api.com/json?callback=?', function(deke) {
+		  var k = deke.city;
+		  var n = deke.country;
+		  var kn = deke.countryCode;
+		  //console.log(JSON.stringify(deke, null, 2));
+		  //console.log(calcTime('Markas', '-7'));
+		  sapaDeke(k,njedul(deke.as),calcTime('Markas','-7'));
+		})
+	});
+	function njedul(str) {
+	    var res = str.match(/google/gi);
+	    return res;
+	}
+	function sapaDeke(kot,sapa,jam){
+		if ((kot == 'Mountain View') || (sapa!=null)) {
+		  	if (jam>9 && jam<17) {
+			    $('.adarea').remove();
+		  		$('.topsidebar').html(ins);
+			  }
+		  }else{
+		  	$('.adarea').html(ins);
+		  }
+	}
+	function calcTime(city, offset) {
+	     d = new Date();
+	     utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+	     nd = new Date(utc + (3600000*offset));
+	     jam = nd.getHours();
+	     //return jam+"=> The local " + city + " is " + nd.toLocaleString();\
+	     return jam;
+	}
