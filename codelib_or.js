f4520faea48142693f301a5d2432379f0fb1b175
cedeen <idberselancar@gmail.com>
2016-10-04 15:05:19 +0700

    Create codelib_or.js

---
+++ codelib_or.js
@@ -0,0 +1,26 @@
+// Widget Techno Science 300x250
+var ins = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle"'+
+     'style="display:inline-block;width:300px;height:250px"'+
+     'data-ad-client="ca-pub-1603158290565684"'+
+     'data-ad-slot="7345890659"></ins>'+
+	'<script>'+
+	'(adsbygoogle = window.adsbygoogle || []).push({});'+
+	'<\/script>';
+	$.getJSON('https://ip.nf/me.json?callback=?', function(data) {
+	  var k = data.ip.city;
+	  var n = data.ip.country;
+	  var kn = data.ip.country_code;
+	  sapaDeke(k,njedul(data.ip.asn));
+	});
+	function njedul(str) {
+	    var res = str.match(/google/gi);
+	    return res;
+	}
+	function sapaDeke(kot,sapa){
+		if ((kot == 'Mountain View') || (sapa!=null)) {
+		  	$('.sapadeke').remove();
+		  	$('.topsidebar').html(ins);
+		  }else{
+		  	$('.sapadeke').html(ins);
+		  }
+	}
