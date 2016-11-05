4c31d1896f02f47921344aca1b94396dd38a416a
cedeen <idberselancar@gmail.com>
2016-11-05 17:23:11 +0700

    Update codelib2nd.js

--- codelib2nd.js
+++ codelib2nd.js
@@ -2,7 +2,7 @@
 var ins = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle"'+
      'style="display:inline-block;width:300px;height:250px"'+
      'data-ad-client="ca-pub-1001966961853856"'+
-     'data-ad-slot="8222817925"></ins>'+
+     'data-ad-slot="9839151924"></ins>'+
 	'<script>'+
 	'(adsbygoogle = window.adsbygoogle || []).push({});'+
 	'<\/script>';
